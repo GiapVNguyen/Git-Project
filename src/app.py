8447680e32318d1375a6b4a56f380451f7f9bc13
Giap V Nguyen <giapvietnguyen@gmail.com>
2026-07-02 10:48:59 +0900

    feat: calculate BMI

--- src/app.py
+++ src/app.py
@@ -1,3 +1,15 @@
 print("Hello, Git")
 
 print("Hello from AI Vietnam")
+print("Hello from AI Vietnam 2")
+
+print("Hello from AI Viet nam 3")
+def phan_loai_bmi(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 24.9:
+        return "Normal weight"
+    elif 25 <= bmi < 29.9:
+        return "Overweight"
+    else:
+        return "Obesity"
