72d4e84ce943b86c61cb754594ca4d71142e6609
Giap V Nguyen <giapvietnguyen@gmail.com>
2026-07-02 10:52:39 +0900

    feat: calculate BMI

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ print("Hello, Git")
 
 print("Hello from AI Vietnam")
 print("Hello from AI Vietnam 2")
-
+ gi
 print("Hello from AI Viet nam 3")
 def phan_loai_bmi(bmi):
     if bmi < 18.5:
@@ -13,3 +13,14 @@ def phan_loai_bmi(bmi):
         return "Overweight"
     else:
         return "Obesity"
+    
+    def main():
+        print("Chương trình tính BMI")
+        weight = float(input("Nhập cân nặng (kg): "))
+        height = float(input("Nhập chiều cao (m): "))
+        bmi = weight / (height ** 2)
+        category = phan_loai_bmi(bmi)
+        print(f"Chỉ số BMI của bạn là: {bmi:.2f}")
+        print(f"Phân loại: {category}")
+    if __name__ == "__main__":
+        main()
